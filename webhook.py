438534bd193f0cda339e118df33f03751762e98d
laika <71929285+01laika@users.noreply.github.com>
2021-02-08 18:49:17 +0000

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -3,19 +3,32 @@ import crayons
 import time
 import json
 
-headers = {"content-type": "application/json"}
+def main():
+    hook = input(crayons.red("what is the webhook url?\t=> "))
 
-hook = input(crayons.red("what is the webhook url?\t=> "))
-content = input(crayons.red("input message to spam\t=> "))
-data = {"content": content}
-try: delay = float(input(crayons.red("delay per each message sent\t=> ")))
-except ValueError:
-    print(crayons.red("put a number in"))
-else:
-    print(crayons.green("[+] spam sent! [+]"))
+    try:
+        r = requests.get(hook)
+        if r.json()["message"]:
+            print(crayons.red("invalid webhook!\n"))
+            time.sleep(1)
+            main()
+    except KeyError:
+        pass
 
-    while True:
-        time.sleep(delay)
-        r = requests.post(hook, headers=headers, json=data)
-        if r.status_code == 429:
-            print(crayons.red("you are being rate limited!"))
+    content = input(crayons.red("input message to spam\t=> "))
+    headers = {"content-type": "application/json"}
+    data = {"content": content}
+    try: delay = float(input(crayons.red("delay per each message sent\t=> ")))
+    except ValueError:
+        print(crayons.red("put a number in"))
+    else:
+        print(crayons.green("{+} spam sent! {+}\nctrl-c to stop!"))
+
+        while True:
+            time.sleep(delay)
+            r = requests.post(hook, headers=headers, json=data)
+            if r.status_code == 429:
+                print(crayons.red("you are being rate limited!"))
+
+if __name__ == "__main__":
+    main()
